24adeb66dace56567a1775a2efbea025d4195720
xiaohuihui0413 <xiaofei_yang@brown.edu>
2017-06-18 09:37:37 +0800

    Add files via upload

---
+++ cachematrix_yxf.R
@@ -0,0 +1,11 @@
+makeVector<-function(x=numeric()){
+m<-NULL
+set<-function(y){
+x<<-y
+m<<-NULL
+}
+get<-function()x
+setinverse<-function(solve)m<<-solve
+getinverse<-function()m
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+}
